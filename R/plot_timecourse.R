3b2ecf284b466aaf29074d3c278d8df149295efe
Drew Steen <andrew.decker.steen@gmail.com>
2014-11-26 12:16:15 -0500

    complete but untested version of plot_timecourse()

--- R/plot_timecourse.R
+++ R/plot_timecourse.R
@@ -24,61 +24,33 @@ plot_timecourse <- function(df,
   df_summ <- ddply(df, exp_var-but-not-replicate, summarise, 
                    mean.y.var=mean(y.var, na.rm=TRUE),
                    sd.y.var=sd(y.var), na.rm=TRUE)
-  # 1. exp_var contains only x.var (seems unlikely; possible for tuneup experiments I guess)
-  p <- ggplot(df, aes(x=x.var, y=y.var)) + 
-    geom_point()
-  # 2. exp_var contains only x.var and replicate
-  exp_var_short <- exp_var[-which(exp_var %in% rep.var)]
   
-  p <- ggplot(df, aes(x=x.var, y=y.var, colour=replicate)) + 
-    geom_point() 
+  # 1. exp_var contains only x.var (seems unlikely; possible for tuneup experiments I guess)
+  if(length(exp_var==1)) { # Note that ethe presence of x.var in exp_var has already been tested
+    p <- ggplot(df, aes(x=x.var, y=y.var)) + 
+      geom_point()
+  }
+ 
+  # 2. exp_var contains only x.var and replicate
+  if(length(exp_var)==2 & sum(c(x.var, rep.var) %in% exp_var) == 2) { # There's got to be a better way to test that exp_var consists of only x.var and rep.var
+    exp_var_short <- exp_var_short[-which(exp_var %in% rep.var)]
+    p <- ggplot(df, aes(x=x.var, y=y.var, colour=replicate)) + 
+      geom_point() 
+  }
+  
   # 3. exp_var contains only x.var and one or more other value, but not replicate
-  p <- ggplot(df, aes(x=x.var, y=y.var)) + 
-    geom_point() + 
-    facet_wrap(~[all the exp.vars except time])
+  if(length(exp_var) >= 2 & !(rep.var %in% exp_var)) {
+    p <- ggplot(df, aes(x=x.var, y=y.var)) + 
+      geom_point() + 
+      generate_facet_expression(exp_var_short)
+  }
   
   # 4. exp_var contains x.var and replicate and one or more other values
-  p <- ggplot(df, aes(x=x.var, y=y.var, colour=rep.var)) + 
-    geom_point() + 
-    facet_wrap(![all the exp vars except replicate and tiem])
+  if(length(exp_var > 2) & rep.var %in% exp_var) {
+    p <- ggplot(df, aes(x=x.var, y=y.var, colour=rep.var)) + 
+      geom_point() + 
+      generate_facet_expression(exp_var_short)
+  }
   
-#   # Case when there is no experimental variable at all. THIS SHOULD BE RARE
-#   if(is.null(exp_var)) {
-#     p <- ggplot(df, aes_string(x=x.var, y=y.var)) + 
-#       geom_point() 
-#   } else { # There exp_var has at least one element
-#     # Case in which there is only replicate (rep.var) in the experimental treatment
-#     if("rep.var" %in% exp_var) {
-#       p <- ggplot(df, aes_string(x=x.var, y=y.var, colour=rep.var)) +
-#         geom_point() 
-#       # Calculate average of replicates
-# #       df_summ <- ddply(df, c(SOMETHING), summarise,
-# #                        mean.y=mean(y.var))
-# #       p <- p+geom_line(data=df_summ, aes_string(x=x.var, y=y.var))
-#     } else {
-#       # remove replicate & time variables from exp_var
-#       exp_var_short <- exp_var[-which(exp_var==rep.var | exp_var==x.var)]
-#       
-#       # TESTING plots with Abigail's example
-#       p <- ggplot(df, aes_string(x=x.var, y=y.var, colour=rep.var)) + 
-#         geom_point() + 
-#         #geom_line(aes_string(group="replicate")) +
-#         facet_wrap(as.formula(paste("compound ~", do.call(paste, c(as.list(exp_var_short), sep="+")))))
-#       
-#     }
-#     
-#  }
-#   
-#   if(!is.null(exp_pred)) {
-#     p <- p + 
-#       geom_line(data=exp_pred, aes_string(x=x.var, y=y.var, colour=treatment))
-#   }
-#   
-#   if(print.plot) {
-#     print(p)
-#   }
-#   
-#   if(save.plot) {
-#     ggsave(fn, p, ...)
-#   }
+  
 }
