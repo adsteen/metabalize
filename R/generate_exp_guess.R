e0c84670dcce4a12f6d58b845a823ebf67b095d4
Drew Steen <andrew.decker.steen@gmail.com>
2014-11-27 11:17:05 -0500

    added generate_exp_guess
    Does not have error checking yet

---
+++ R/generate_exp_guess.R
@@ -0,0 +1,23 @@
+##' Generate guesses for A and k in exponential fit
+##' 
+##' Fits a linear least-squares fit to log-transformed exponential data
+generate_exp_guess <- function(x, y) {
+  
+  # Log transform the y data
+  log.y <- log(y)
+  
+  # Create linear model of log-transformed data
+  ### MUST WRAP THIS IN TRY.CATCH
+  lin.mod <- lm(log.y ~ x)
+  
+  # y = A exp^(kx)
+  # log(Y) = log(A) + kx
+  # k = slope; A = exp(intercept)
+  
+  A_guess <- exp(coef(lin.mod)[1])
+  k_guess <- coef(lin.mod)[2]
+  
+  # Return vector of guesses
+  c("A_guess" = A_guess, "k_guess" = k_guess)
+  
+}
