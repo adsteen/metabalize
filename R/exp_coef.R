f9c226c4922fe69cb2efb1b96fdd4c3ebaf4d3d0
Drew Steen <andrew.decker.steen@gmail.com>
2014-12-08 00:02:25 -0500

    Untested version of exp_coef
    returns a vector of the coefficients of an exponential model

---
+++ R/exp_coef.R
@@ -0,0 +1,34 @@
+##' Returns parameters of an exponential model
+##' Right now only works with two-factor exponential models
+exp_coef <- function(mod) {
+  
+  # Check whether model is an nls model
+  if(class(mod) != "nls") {
+    rep(NA, 5)
+  }
+  
+  # Get the coefficients
+  coefs <- summary(mod)$coefficients[ , 1:2]
+  coefs.vec <- as.vector(coefs)
+  
+  # Test whether there are more than two rows: this could be trouble
+  if(nrow(coefs) != 2) {
+    rep(NA, 5)
+    warning("This model did not have exactly two fitted terms")
+  }
+  
+  # Get the number of points
+  n <- length(resid(mod))
+  coefs.vec[5] <- n
+  
+  # Name the vector
+  rn <- rownames(coefs)
+  coefs.names <- c(paste(rn[1], ".est", sep=""),
+                   paste(rn[2], ".est", sep=""),
+                   paste(rn[1], "std.err", sep=""), 
+                   paste(rn[2], "std.err", sep=""),
+                   "n")
+  names(coefs.vec) <- coefs.names
+  
+  coefs.vec
+}
