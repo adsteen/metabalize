1445cadba536ce21c7ce7db3bd84693887254e04
Drew Steen <andrew.decker.steen@gmail.com>
2014-12-21 00:48:04 -0500

    wrote metabolize package documentation

--- R/metabalize.R
+++ R/metabalize.R
@@ -1,1 +1,10 @@
+##' Process metabolomic pulse-chase flux experiments
+##' 
+##' metabalize allows users to automatically input data output from MAVEN, identify 12C isotopomers, fit exponential functions to the data and plot and export those fits.
+##' 
+##' See (some vignette) for details 
+##' 
+##' @docType package
+##' @name metabalize
+
 NULL
