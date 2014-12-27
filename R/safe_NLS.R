4c7921047e5afadc38616afe2ec34ba158003f75
Drew Steen <andrew.decker.steen@gmail.com>
2014-12-27 17:03:39 -0500

    send xvar and yvar to generate_exp_guess

--- R/safe_NLS.R
+++ R/safe_NLS.R
@@ -43,7 +43,7 @@ safe_NLS <- function(df, xvar="time", yvar="relative.ion.count") {
   yvals <- df[ , yvar]
   
   # Generate guesses for exponential fits
-  guesses <- generate_exp_guess(xvals, yvals)
+  guesses <- generate_exp_guess(xvals, yvals, xvar, yvar)
   
   if(is.null(guesses)) {
     return(NA)
