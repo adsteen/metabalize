6b15d84b01801b48ba1791ecdccc49b0c1c0eaa9
Drew Steen <andrew.decker.steen@gmail.com>
2015-02-07 08:38:05 -0500

    added code to check for errors in sample key

--- R/read_MAVEN.R
+++ R/read_MAVEN.R
@@ -4,12 +4,15 @@
 ##' @param parse_col_names Not yet implemented
 ##' @param key_fn Filename (including path) of the key file
 ##' @param id.cols I can't remember what this does
+##' @param append.X appends X to the beginning of sample names - I need to handle this automatically
 ##' @export
 read_MAVEN <- function(data_fn, parse_col_names=FALSE, key_fn, id.cols=NULL) {
+  
+  # Read the raw data
   d <- read.csv(data_fn)
   
-  #browser()
   # Someday I would like to implement a column name parser, but I won't do this just yet
+  # Read the sample key
   if(parse_col_names) {
     stop("The functionality to parse column names automatically has not yet been implemented.")
   } else {
@@ -19,12 +22,19 @@ read_MAVEN <- function(data_fn, parse_col_names=FALSE, key_fn, id.cols=NULL) {
     key <- read.csv(key_fn)
   }
   #browser()
+  
   # Determine experimental variables
   exp.var <- names(key)[2:length(names(key))] # experimental variables are everything in the sample key except the key name
   
   #KLUGE TO FIX:
+  browser()
   key$sample <- as.factor(paste("X", key$sample, sep=""))
   
+  # Check for mismatch between key names and sample names
+  if(sum(unique(d$sample) %in% unique(key$sample)) < length(unique(d$sample))) {
+    warning("Some samples listed in the raw dataset are not listed in the sample key. /n
+            What's worse, no one has implemented code to tell you _which_ samples are missing from the key, even though it would be easy to do so.")
+  }
   
  
   #
@@ -40,6 +50,7 @@ read_MAVEN <- function(data_fn, parse_col_names=FALSE, key_fn, id.cols=NULL) {
   
   # Remove the rows where everything is NA
   dm <- dm[!is.na(dm$ion.count), ]
+
   
   # Merge key with data
   d_merge <- merge(dm, key, by="sample") # There's a problem with the sample names in key
